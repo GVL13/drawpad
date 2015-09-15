346292ff5e0981813dd621a1e8d7e84bbf960073
GVL <gregvanlunteren@gmail.com>
2015-09-15 13:33:01 -0400

    got the grid generator working

--- drawpad.js
+++ drawpad.js
@@ -8,10 +8,21 @@ function posttest() {
 }
 */	
 function gridmaker() {
-	pixInput = parseInt(document.getElementById("pixels").value);
-	for (var i = 0; i < pixInput; i++) {
+	var pixInput = parseInt(document.getElementById("pixels").value);
+	var totalPix = Math.pow(pixInput, 2)/2;
+	for (var i = 0; i < totalPix; i++) {
 		$("#big-ass-grid").append("<div class='pix-box'></div>")
-		// something to make buttons disappear or otherwise deactivate-else it'll fuck up
-	}
-	$(".box").addclass("pix-box");
+	};
+
+	var calc_width = ((1200-(2*pixInput))/pixInput);
+	window.alert(calc_width)
+	$(".pix-box").css("width", calc_width+"px");
+	$(".pix-box").css("height", calc_width+"px");
+	// something here to make buttons disappear or otherwise deactivate-else it'll fuck up
 }
+
+/*
+function gridlines() {
+	$(".pix-box").css("border-style", "dashed");
+}
+*/
